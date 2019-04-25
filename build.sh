f720d430db2d0b5000a911bab46b79e982b32947
Pradeep Sathasivam <pradeep.sathasivam@hpe.com>
2019-04-25 16:47:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 svc_status=$(docker-compose ps |  grep "userssvc" | awk '{ print $4}')
 echo $svc_status
-if [ $svc_status != "Up" ]
+if [ "$svc_status" != "Up" ]
 then
    echo "User service is not up"
    exit 1
@@ -10,14 +10,14 @@ port=$(docker-compose ps |  grep "userssvc" | awk '{ print $5}' | awk '{split($0
 echo $port
 add_status=$(curl -s -o /dev/null -w "%{http_code}"  -X PUT -X PUT http://localhost:$port/user -H "Content-Type: application/json" -d '{"emailId":"pradeeep@abc.com","displayName":"Pradeep S","password":"test123"}' | grep "202")
 echo $add_status
-if [[ $add_status != "202" ]]
+if [[ "$add_status" != "202" ]]
 then
   echo "curl request failed for add user"
   exit 1
 fi
 status=$(curl -s --head --request GET http://localhost:$port/user | grep "200 OK")
 echo $status
-if [[ $status != *"200 OK"* ]]
+if [[ "$status" != *"200 OK"* ]]
 then
   echo "curl request failed."
   exit 1
